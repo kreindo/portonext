d0dac9e9c51a074e056b30d0de0a99d5d7a7e64d
kreindo <ahmadsan4p@gmail.com>
2022-07-09 07:29:33 +0800

    added pages, and routes

--- pages/components/header.jsx
+++ pages/components/header.jsx
@@ -1,34 +1,54 @@
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 import Logo from '../../public/assets/images/logo1.png';
+import Link from 'next/link';
 
-export const header = () => {
-  const menus = ['Design', 'Website', 'Profil Santri'];
+const Header = () => {
+  // const [menu, setMenu] = useState('menu');
+  // const menus = ['Design', 'Website', 'Profil Santri'];
   const handleClick = () => {
     console.log('hello');
   };
+  // useEffect(() => setMenu(menu), []);
+
   return (
-    <div className="flex flex-1 my-14">
+    <div className="flex flex-1 my-14 mx-16">
       <Image
         src={Logo}
         alt="Logo"
-        width={307}
-        height={64}
         className="hover:cursor-pointer"
         onClick={handleClick}
         z
       />
-
       <div>
-        {menus.map((menu) => {
+        <ul className="flex">
+          <li className="mx-10">
+            <Link href="/design">
+              <a>Design</a>
+            </Link>
+          </li>
+          <li className="mx-10">
+            <Link href="/website">
+              <a>Website</a>
+            </Link>
+          </li>
+          <li className="mx-10">
+            <Link href="/profile">
+              <a>Profil Santri</a>
+            </Link>
+          </li>
+        </ul>
+
+        {/* {menus.map((menu) => {
           return (
             <li key={menu}>
               <a href="">{menu}</a>
             </li>
           );
-        })}
+        })} */}
       </div>
     </div>
   );
 };
 
-export default header;
+export default Header;
